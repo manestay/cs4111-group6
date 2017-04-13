8fc4dfe6b5f9227f2868bc7c89ee0797ea04c2ae
Sophie Lucy <sophiejlucy@gmail.com>
2017-04-13 11:50:46 -0400

    switch fval and pval

--- server.py
+++ server.py
@@ -188,15 +188,19 @@ def results():
         if query1[-5:] == 'WHERE': query1 = query1.replace('WHERE', '')
         per = g.conn.execute(query1)
         for i in per: 
-          r1.append(i['ename'] + "\t" + i[0] + "% Off" + " " + i['notes'] + " " + i['sname'])
+          r1.append(i['ename'] + " " + str(i['percent']) + "% Off" + " " + i['notes'] + " " + i['sname'])
 
     fv = g.conn.execute(query)
     # for row in ename:
     #   print row
     
     for i in fv: 
-      r.append(i['ename'] + "\t" + " Student Price: $" + i[0] + " " + i['notes'] + " " + i['sname'])
-    return render_template("results.html", pval = r, fval = r1)
+      if i['student_price']==0:
+        student = 'free'
+      else:
+        student = '$' + str(i['student_price'])
+      r.append(i['ename'] + " " + " Student Price: " + student + " " + i['notes'] + " " + i['sname'])
+    return render_template("results.html", fval = r, pval = r1)
 
 
 @app.route('/account', methods=['GET', 'POST'])
